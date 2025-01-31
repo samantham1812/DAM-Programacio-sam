613cf6f307816431d236252ce96cea96612d5a75
samantham1812 <150824201+samantham1812@users.noreply.github.com>
2025-01-31 18:38:04 +0100

    .

--- Java-R/Deures/Deures02/src/main/java/com/exercici0200/Estudiant.java
+++ Java-R/Deures/Deures02/src/main/java/com/exercici0200/Estudiant.java
@@ -1,10 +1,14 @@
 package com.exercici0200;
 
 public class Estudiant {
+
+    private String nom;
+    private String id;
     
     // Constructor
     public Estudiant(String nom, String id) {
-        
+        this.nom = nom;
+        this.id = id;
     }
 
     // Mètodes públics estàtics
