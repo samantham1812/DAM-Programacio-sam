ae17c5bc912f62d418277c6de45d47b85863e178
samantham1812 <150824201+samantham1812@users.noreply.github.com>
2025-02-07 18:13:39 +0100

    co

--- Java-R/Deures/Deures02/src/main/java/com/exercici0200/Estudiant.java
+++ Java-R/Deures/Deures02/src/main/java/com/exercici0200/Estudiant.java
@@ -11,8 +11,26 @@ public class Estudiant {
         this.id = id;
     }
 
+    /* Getters i Setters */
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     // Mètodes públics estàtics
     public static int getComptadorEstudiants() {
+        
         return -1;
     }
 
