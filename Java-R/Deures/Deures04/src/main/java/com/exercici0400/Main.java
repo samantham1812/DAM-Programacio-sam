b523805becb9256dbde21473de2aed6854f5a2c2
samantham1812 <150824201+samantham1812@users.noreply.github.com>
2025-02-20 16:07:33 +0100

    cambi

--- Java-R/Deures/Deures04/src/main/java/com/exercici0400/Main.java
+++ Java-R/Deures/Deures04/src/main/java/com/exercici0400/Main.java
@@ -1,6 +1,7 @@
 package com.exercici0400;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import javax.print.attribute.standard.MediaPrintableArea;
@@ -19,11 +20,11 @@ public class Main {
         Text txt2 = new Text(22, 6 , 4, 3, "abcdefghijkl", 3, false, "left");
         */
 
-        HashMap<Integer, MenuItem> mapMnu = new HashMap<>();
+        // HashMap<Integer, MenuItem> mapMnu = new HashMap<>();
         //String[] arr0 = {"Sortit", "sortir", "exit"};
-        mapMnu.put(0, new MenuItem("Sortir", new String[] {"sortir","exit"}));
-        mapMnu.put(1, new MenuItem("Alinear esquerra", new String[] {"esquerra","left"}));
-        mapMnu.put(2, new MenuItem("Alinear dreta", new String[] {"dreta","right"}));
+        // mapMnu.put(0, new MenuItem("Sortir", new String[] {"sortir","exit"}));
+        // mapMnu.put(1, new MenuItem("Alinear esquerra", new String[] {"esquerra","left"}));
+        /*mapMnu.put(2, new MenuItem("Alinear dreta", new String[] {"dreta","right"}));
         mapMnu.put(3, new MenuItem("Alineació centrada", new String[] {"centrat","center"}));
 
         //String[] arr1 = {"Alinear esquerra", "esquerra", "left"};
@@ -32,14 +33,26 @@ public class Main {
         mapMnu.put(2, arr2);
         String[] arr3 = {"Alinear centrado", "centrado", "center"};
         mapMnu.put(3, arr3);*/
-
+        /* 
         Menu mnu0 = new Menu(1,1,20,8, "Titol Menu", mapMnu,false);
         Menu mnu1 = new Menu(22,1,25,8, "Titol Menu", mapMnu,true);
 
         ArrayList<Component> components = new ArrayList<>();
         components.add(mapMnu);
         //components.add(mnu1);
-        /*components.add(txt2);*/
+        components.add(txt2);*/
+
+        ArrayList<String> tabHeaders0 = new ArrayList<>(Arrays.asList("col0", "col1","col2"));
+        ArrayList<Integer> tabWidth = new ArrayList<>(Arrays.asList(5,8,10));
+        ArrayList<String> tabAligns0 = new ArrayList<>(Arrays.asList("left","right","center"));
+        ArrayList<ArrayList<String>> tabRows0 = new ArrayList<>(Arrays.asList(
+            new ArrayList<>(Arrays.asList("ab", "si", "gos")),
+            new ArrayList<>(Arrays.asList("def", "no", "guineu")),
+            new ArrayList<>(Arrays.asList("hi", "perque", "llop")),
+            new ArrayList<>(Arrays.asList("jklm", "qui", "formiga"))
+        ));
+
+        Table tab0 = new Table(0, 0, 28, 8, "Taula", tabHeaders0, tabWidth, tabAligns0, tabRows0);
 
         Container container = new Container(50, 10, components);
         container.draw();
