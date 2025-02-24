2fe71015e445b0d9bce4f88337f4bcf850711e7d
samantham1812 <150824201+samantham1812@users.noreply.github.com>
2025-02-24 17:03:42 +0100

    tabla con cabeceras y texto

--- Java-R/Deures/Deures04/src/main/java/com/exercici0400/Table.java
+++ Java-R/Deures/Deures04/src/main/java/com/exercici0400/Table.java
@@ -17,30 +17,86 @@ public class Table extends Component {
         this.rows = rows;
     }
 
+    public static String alineacio (String txt, int width, String align) {
+        
+        String truncated = txt;
+        if (txt.length() < width) {
+            truncated = txt;
+        } else {
+            truncated = txt.substring(0, width);
+        }
+
+        int dist = width - txt.length();
+        String spaces = " ".repeat(dist);
+
+        if (align.contentEquals("left")) {
+            return truncated + spaces;
+        } else if (align.contentEquals("right")) {
+            return spaces + truncated;
+        } else{
+            int left = (dist / 2) + (dist % 2);
+            int right = dist - left;
+            return " ".repeat(left) + truncated + " ".repeat(right);
+        }
+    }
+
     public ArrayList<String> render() {
         ArrayList<String> rst = new ArrayList<String>();
 
         // Afegir linia buida al principi
         rst.add(0, " ".repeat(width)); 
 
-        String capcelera = "";
-        for (int cnt = 0; cnt < headers.size(); cnt = cnt + 1) {
-            String header = headers.get(cnt);
-            int width = widths.get(cnt);
-
-            capcelera = capcelera + header;
+        String cap = "";
+        for (int i = 0; i < headers.size(); i++) {
+            String header = headers.get(i);
+            int width = widths.get(i);
+            String align = aligns.get(i);
             
-            if (cnt < (headers.size() - 1)) {
-                capcelera = capcelera + "|";
+            header = alineacio(header, width, align);
+
+            cap = cap + header;
+            if (i < headers.size() - 1) {
+                cap = cap + "|";
             }
         }
-        rst.add(1, " " + capcelera + " ");
+        rst.add(1, " " + cap + " ".repeat(width - cap.length() - 1));
 
-
-        for (int i = 2; i < height; i++) {
+        String separador="";
+        for (int i = 0; i < widths.size(); i++) {
+            int width = widths.get(i);
+            
+            if (i == widths.size()-1) {
+                separador = separador + "┼" + "─".repeat(width + (super.width - cap.length()));
+            }
+            else{
+                separador = separador + "┼" + "─".repeat(width);
+            }
+        }
+        rst.add(2, separador);
+        // rst.add(2, "│" + "─".repeat(widths.get(0)) + "┼" + "─".repeat(widths.get(1)) + "┼" + "─".repeat(widths.get(2) + 1) + "│");
+        
+        //for (int cntcol = 3; cntcol < height; cntcol++){}
+        for (ArrayList<String> row : rows) {
+            String cap2 = "";
+                //Integer num = 3;
+            for (int i = 0; i < row.size(); i++){
+                String item = row.get(i);
+                int width = widths.get(i);
+                String align = aligns.get(i);
+            
+                item = alineacio(item, width, align);    
+                cap2 = cap2 + item;
+                if (i < headers.size() - 1) {
+                    cap2 = cap2 + "|";
+                }
+            }
+            rst.add(rst.size(), " " + cap2 + " ".repeat(width - cap.length() - 1));
+        }
+           
+        // Asignar text segons alineació 
+        for (int i = 7; i < height; i++) {
             rst.add(i, "x".repeat(width));
         }
-
         return rst;
     } 
 }
