cf4fd66bb63e5aa0042d9886ce7de9305f47ffaf
samantham1812 <150824201+samantham1812@users.noreply.github.com>
2025-03-10 17:33:07 +0100

    base

--- Java-R/Teoria/src/main/java/com/exercici1400/Biblioteca.java
+++ Java-R/Teoria/src/main/java/com/exercici1400/Biblioteca.java
@@ -1,5 +1,7 @@
 package com.exercici1400;
 
+import java.lang.reflect.Array;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Locale;
@@ -10,6 +12,7 @@ public class Biblioteca {
         AppData db = AppData.getInstance();
 
         db.update("DROP TABLE IF EXISTS Editorials");
+
         String sql = """
             CREATE TABLE IF NOT EXISTS Editorials (
                 id_editorial INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -21,41 +24,48 @@ public class Biblioteca {
 
     public static void crearTaulaLlibres() {
         AppData db = AppData.getInstance();
-
         db.update("DROP TABLE IF EXISTS Llibres");
 
-        String sql = """
-            CREATE TABLE IF NOT EXIST Llibres (
-                id_llibre INTEGER PRIMARY KEY AUTOINCREMENT,
-                titol TEXT,
-                autor TEXT,
-                any_publicacio INTEGER,
-                id_editorial INTEGER,
-                FOREIGN KEY(id_editorial) REFERENCES Editorials(id_editorial))";
-            )
-        """;
-       AppData.getInstance().update(sql);
+        String sql = "CREATE TABLE IF NOT EXISTS Llibres (" +
+            " id_llibre INTEGER PRIMARY KEY AUTOINCREMENT," +
+            " titol TEXT NOT NULL," +
+            " autor TEXT," +
+            " any_publicacio INTEGER," +
+            " id_editorial INTEGER," +
+            " FOREIGN KEY(id_editorial) REFERENCES Editorials(id_editorial))";
+        AppData.getInstance().update(sql);
     }
 
     public static void afegirEditorial(String nom) {
-        // TODO 
-        String sql = String.format("", nom);
-        AppData.getInstance().update(sql);
+        AppData db = AppData.getInstance();
+        String sql = String.format("INSERT INTO Editorials (nom) VALUES ('%s')", nom);
+        db.update(sql);
     }
 
     public static void afegirLlibre(String titol, String autor, int anyPublicacio, int idEditorial) {
-         // TODO 
-        String sql = String.format("", titol, autor, anyPublicacio, idEditorial
-        );
-        AppData.getInstance().update(sql);
+        AppData db = AppData.getInstance();
+        String sql = String.format("INSERT INTO Llibres (titol, autor, any_publicacio, id_Editorial) VALUES ('%s', '%s', %d, %d);", titol, autor, anyPublicacio, idEditorial);
+        db.update(sql);
     }
 
     public static void llistarTaulaEditorials() {
-        // TODO 
+        AppData db = AppData.getInstance();
+        
+        List<HashMap<String, Object>> Editorials = db.query("SELECT * FROM Editorials;");
+
+        for (HashMap<String,Object> editorial : Editorials) {
+            System.out.println(editorial.get("id_editorial") + ". " + editorial.get("nom"));
+        }
     }
 
     public static void llistarTaulaLlibres() {
-        // TODO 
+        AppData db = AppData.getInstance();
+
+        List<HashMap<String, Object>> Llibres = db.query("SELECT * FROM Llibres;");
+
+        for (HashMap<String,Object> llibre : Llibres) {
+            System.out.println(llibre.get("id_llibre") + ". " + llibre.get("titol") + " " +  llibre.get("autor") + ", " + llibre.get("any_publicacio") + ", " + llibre.get("id_editorial"));
+        }
     }
 
     public static void llistarInfoLlibre(int idLlibre) {
