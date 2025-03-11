1279c94cc3e0f3a2178d9145846b9329e70ff0e9
samantham1812 <150824201+samantham1812@users.noreply.github.com>
2025-03-11 17:58:49 +0100

    cosas

--- Java-R/Deures/Deures05/src/main/java/com/exercici0500/Cinema.java
+++ Java-R/Deures/Deures05/src/main/java/com/exercici0500/Cinema.java
@@ -24,18 +24,18 @@ public class Cinema {
 
     public static void crearTaulaPelis() {
         AppData db = AppData.getInstance();
+
         db.update("DROP TABLE IF EXISTS pelis");
-        String sql = """
-            CREATE TABLE IF NOT EXISTS pelis (
-                id_peli INTEGER PRIMARY KEY AUTOINCREMENT,
-                titol TEXT,
+
+        db.update("""
+                CREATE TABLE IF NOT EXISTS pelis(
+                id_peli INT AUTO_INCREMENT PRIMARY KEY,
+                titol TEXT NOT NULL,
                 any_estrena INTEGER,
                 durada INTEGER,
                 id_director INTEGER,
                 FOREIGN KEY(id_director) REFERENCES directors(id_director))
-            );
-        """;
-        db.update(sql);
+                """);
     }
 
     public static void crearTaulaSales() {
@@ -44,10 +44,10 @@ public class Cinema {
         String sql = """
             CREATE TABLE IF NOT EXISTS sales (
                 id_sala INTEGER PRIMARY KEY AUTOINCREMENT,
-                nom_sala TEXT,
-                capacitat INTEGER,
+                nom_sala TEXT NOT NULL,
+                capacitat INTEGER NOT NULL,
                 id_peli INTEGER,
-                FOREIGN KEY(id_peli) REFERENCES pelis(id_peli))
+                FOREIGN KEY(id_peli) REFERENCES pelis(id_peli)
             );
         """;
         db.update(sql);
@@ -60,15 +60,21 @@ public class Cinema {
      * @return l'identificador del director afegit
      */
     public static int afegirDirector(String nomDirector, String nacionalitat) {
-        return 0;
+        AppData db = AppData.getInstance();
+        String sql = String.format("INSERT INTO directors (nom, nacionalitat) VALUES ('%s','%s')",nomDirector,nacionalitat);
+        return db.insertAndGetId(sql);
     }
 
     public static int afegirPeli(String titol, int any, int duracio, int idDirector) {
-        return 0;
+        AppData db = AppData.getInstance();
+        String sql = String.format("INSERT INTO pelis (titol, any_estrena, durada, id_director) VALUES ('%s','%d','%d','%d')", titol, any_estrena, durada, id_director);
+        return db.insertAndGetId(sql);
     }
 
     public static int afegirSala(String nomSala, int capacitat, int idPeli) {
-        return 0;
+        AppData db = AppData.getInstance();
+        String sql = String.format("INSERT INTO sales (nom_sala, capacitat, id_peli) VALUES ('%s', %d, %d)", nomSala, capacitat, idPeli);
+        return db.insertAndGetId(sql);
     }
 
     /**
