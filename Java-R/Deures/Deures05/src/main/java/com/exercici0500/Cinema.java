c9adf44a5c4924a2c10904719e8dff93ced84c72
samantham1812 <150824201+samantham1812@users.noreply.github.com>
2025-03-11 16:52:12 +0100

    cinema

--- Java-R/Deures/Deures05/src/main/java/com/exercici0500/Cinema.java
+++ Java-R/Deures/Deures05/src/main/java/com/exercici0500/Cinema.java
@@ -2,6 +2,7 @@ package com.exercici0500;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.sql.*;
 
 public class Cinema {
 
@@ -9,12 +10,47 @@ public class Cinema {
      * Crea la taula "directors", si ja existeix primer l'esborra
      */
     public static void crearTaulaDirectors() {
+        AppData db = AppData.getInstance();
+        db.update("DROP TABLE IF EXISTS directors");
+        String sql = """
+            CREATE TABLE IF NOT EXISTS directors (
+                id_director INTEGER PRIMARY KEY AUTOINCREMENT,
+                nom TEXT,
+                nacionalitat TEXT
+            );
+        """;
+        db.update(sql);
     }
 
     public static void crearTaulaPelis() {
+        AppData db = AppData.getInstance();
+        db.update("DROP TABLE IF EXISTS pelis");
+        String sql = """
+            CREATE TABLE IF NOT EXISTS pelis (
+                id_peli INTEGER PRIMARY KEY AUTOINCREMENT,
+                titol TEXT,
+                any_estrena INTEGER,
+                durada INTEGER,
+                id_director INTEGER,
+                FOREIGN KEY(id_director) REFERENCES directors(id_director))
+            );
+        """;
+        db.update(sql);
     }
 
     public static void crearTaulaSales() {
+        AppData db = AppData.getInstance();
+        db.update("DROP TABLE IF EXISTS sales");
+        String sql = """
+            CREATE TABLE IF NOT EXISTS sales (
+                id_sala INTEGER PRIMARY KEY AUTOINCREMENT,
+                nom_sala TEXT,
+                capacitat INTEGER,
+                id_peli INTEGER,
+                FOREIGN KEY(id_peli) REFERENCES pelis(id_peli))
+            );
+        """;
+        db.update(sql);
     }
 
     /**
