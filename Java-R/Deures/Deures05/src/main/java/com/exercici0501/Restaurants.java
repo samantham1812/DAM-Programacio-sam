859f1a807ee6925e4519dc21f3c5aac2ac7d9544
samantham1812 <150824201+samantham1812@users.noreply.github.com>
2025-03-14 18:10:51 +0100

    creación de tablas y funciones de añadir
    Se crean las tablas y las funciones para añadir

--- Java-R/Deures/Deures05/src/main/java/com/exercici0501/Restaurants.java
+++ Java-R/Deures/Deures05/src/main/java/com/exercici0501/Restaurants.java
@@ -9,21 +9,62 @@ public class Restaurants {
      * si ja existeixen primer les esborra
      */
     public static void crearTaules() {
-
+        AppData db = AppData.getInstance();
+        db.update("DROP TABLE IF EXISTS restaurants");
+        String sql = """
+            CREATE TABLE IF NOT EXISTS restaurants (
+                id_restaurant INTEGER PRIMARY KEY UNIQUE,
+                name TEXT NOT NULL,
+                kind TEXT NOT NULL,
+                tables INTEGER NOT NULL,
+                pricing TEXT NOT NULL
+            );
+        """;
+        db.update(sql);
+        db.update("DROP TABLE IF EXISTS clients");
+        String sql1 = """
+            CREATE TABLE IF NOT EXISTS clients (
+                id_client INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                birth DATE NOT NULL,
+                isVIP BOOLEAN NOT NULL
+            );
+        """;
+        db.update(sql1);
+        db.update("DROP TABLE IF EXISTS services");
+        String sql2 = """
+            CREATE TABLE IF NOT EXISTS services (
+                id_servei INTEGER PRIMARY KEY AUTOINCREMENT,
+                id_restaurant INTEGER NOT NULL,
+                id_client INTEGER NOT NULL,
+                date DATE NOT NULL,
+                expenditure REAL NOT NULL,
+                FOREIGN KEY (id_restaurant) REFERENCES restaurants(id_restaurant),
+                FOREIGN KEY (id_client) REFERENCES clients(id_client)
+            );
+        """;
+        db.update(sql2);
     }
 
     /**
      * Afegeix un restaurant a la base de dades
      */
     public static void addRestaurant(int idRestaurant, String name, String kind, int tables, String pricing) {
+        AppData db = AppData.getInstance();
+        String sql = String.format("INSERT INTO directors (id_restaurant, name, kind, tables, pricing ) VALUES ('%d', '%s','%s','%d','%s')",idRestaurant,name,kind,tables,pricing);
+        db.update(sql);
     }
 
     public static int addClient(String name, String birth, boolean isVIP) {
-        return 0;
+        AppData db = AppData.getInstance();
+        String sql = String.format("INSERT INTO clients (name, birth, isVIP) VALUES ('%s', ?, ?)",name,birth,isVIP);
+        return db.insertAndGetId(sql);
     }
 
     public static int addService(int idRestaurant, int idClient, String date, double expenditure) {
-        return 0;
+        AppData db = AppData.getInstance();
+        String sql = String.format("INSERT INTO services (id_restaurant, id_client, date, expenditure) VALUES ('%d','%d','%s','%f')",idRestaurant,idClient,date,expenditure);
+        return db.insertAndGetId(sql);
     }
 
     /** 
@@ -39,7 +80,8 @@ public class Restaurants {
      * - S'ha afegit un nou 'servei' amb 'id': 5
      */
     public static void loadData(String filePath) {
-
+        AppData db = AppData.getInstance();
+        
     }
 
     /**
