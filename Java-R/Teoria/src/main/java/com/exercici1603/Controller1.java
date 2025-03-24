622cbb5c1526f7f7f0b40512c87aea62fbb2495d
samantham1812 <150824201+samantham1812@users.noreply.github.com>
2025-03-24 21:06:05 +0100

    cambi

--- Java-R/Teoria/src/main/java/com/exercici1603/Controller1.java
+++ Java-R/Teoria/src/main/java/com/exercici1603/Controller1.java
@@ -4,8 +4,36 @@ import com.utils.*;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 public class Controller1 {
+
+    @FXML
+    private AnchorPane container;
+
+    @FXML
+    private TextField nom;
+
+    @FXML
+    private TextField activitat;
+
+    @FXML
+    private TextField datanaixement;
+
+    @FXML
+    private TextField txtexplicatiu;
+
+    @FXML
+    private Button savebut;
+
+    @FXML
+    private Button toficha;
+
+    @FXML
+    private ImageView imagen;
 
     @FXML
     private void animateToView0(ActionEvent event) {
