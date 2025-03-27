d19ad852d6dc8d1dab0c7bdc5abb24707360df44
samantham1812 <150824201+samantham1812@users.noreply.github.com>
2025-03-27 17:09:34 +0100

    cam

--- Java-R/Deures/Deures06/src/main/java/com/exercici0601/ControllerCharacters.java
+++ Java-R/Deures/Deures06/src/main/java/com/exercici0601/ControllerCharacters.java
@@ -11,6 +11,11 @@ import org.json.JSONObject;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
@@ -20,6 +25,25 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class ControllerCharacters {
+
+    @FXML
+    private Label nom;
+
+    @FXML
+    private ImageView img;
+
+    public void setNom(String nom) {
+        this.nom.setText(nom);
+    }
+    public void setImatge(String imagePath) {
+        try {
+            Image image = new Image(getClass().getResourceAsStream(imagePath));
+            this.img.setImage(image);
+        } catch (NullPointerException e) {
+            System.err.println("Error loading image asset: " + imagePath);
+            e.printStackTrace();
+        }
+    }
 
     @FXML
     private void toViewMain(MouseEvent event) {
