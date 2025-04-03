749926dcdea7de92d95e338f145f3faab833accc
samantham1812 <150824201+samantham1812@users.noreply.github.com>
2025-04-03 15:44:02 +0200

    cam

--- Java-R/Deures/Deures06/src/main/java/com/exercici0602/ControllerPokeCard.java
+++ Java-R/Deures/Deures06/src/main/java/com/exercici0602/ControllerPokeCard.java
@@ -3,8 +3,10 @@ package com.exercici0602;
 import com.utils.*;
 
 import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -15,7 +17,7 @@ import javafx.event.ActionEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-public class ControllerPokeCard {
+public class ControllerPokeCard implements Initializable {
 
     @FXML
     private Label labelAbility = new Label();
@@ -28,6 +30,25 @@ public class ControllerPokeCard {
     private ImageView imgPokemon;
 
     private int number;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            // Cargar la imagen del botón de retroceso
+            URL imageURL = getClass().getResource("/assets/images0602/arrow-back.png");
+            if (imageURL != null) {
+                Image image = new Image(imageURL.toExternalForm());
+                imgBackArrow.setImage(image);
+            } else {
+                System.err.println("No se encontró la imagen de flecha.");
+            }
+
+            // Cargar la lista de personajes al inicializar
+            loadList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public void loadPokemon(int number) {
         this.number = number;
